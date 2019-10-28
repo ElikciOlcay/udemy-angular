9bab62d6b927ab9f4338a01e19cadbaddcd7e11b
Olcay Elikci <olcayelikci@Olcays-MacBook-Pro.local>
2019-10-28 23:44:20 +0100

    dependeny injection

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +13,7 @@ export class LoginComponent implements OnInit {
   errorMessage = 'invalid';
   invalidLogin = false;
 
-  constructor() { }
+  constructor(router: Router) { }
 
   ngOnInit() {
   }
